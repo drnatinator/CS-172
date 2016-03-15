ca68b3d59eb021634e875a4d018f9849f46362db
drnatinator <natetheswimmer@gmail.com>
2016-03-15 13:26:32 -0700

    Updated version

--- HW03/HW03_01/HW03/Source.cpp
+++ HW03/HW03_01/HW03/Source.cpp
@@ -7,7 +7,14 @@ using namespace std;
 #include <stack>
 #include <string>
 
+void isPalindrome();
+
 int main() {
+	isPalindrome();
+	return 0;
+}
+
+void isPalindrome() {
 	string input, output;
 	int size;
 
@@ -20,7 +27,7 @@ int main() {
 	for (int i = 0; i < size; i++) {
 		input[i] = tolower(input[i]); //converts the string to lowercase, making the program case-insensitive
 		invert.push(input[i]); //puts each character into a stack, one at a time
-		}
+	}
 	for (int i = 0; i < size; i++) {
 		output += invert.top(); //pulls the characters out of the stack, one at a time, inverting the string
 		invert.pop(); //deletes the top character on the stack, allowing the loops to access the one below it
@@ -28,8 +35,8 @@ int main() {
 	if (input == output) // checks to see if the input string, changed to lowercase is the same as the inverted one
 		cout << "The input is a palindrome!\n";
 	else
-		cout << "The input is not a palindrome. :(\n";	
-	return 0;
+		cout << "The input is not a palindrome. :(\n";
+
 }
 
 
